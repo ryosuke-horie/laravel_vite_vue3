a46335ee27ff92b58a7ed821a522389ceef76493
fsworks\r-horie <r-horie@mamiya-its.co.jp>
2023-09-20 14:51:04 +0900

    :sparkles: viteのvueプラグインを追加して設定に追記

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    server: {
+        // 外部公開
+        host: '0.0.0.0',
+        // 立ち上げる際のポートを変更できます。
+        port: 5173
+    }
 });
